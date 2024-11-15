589b33174cfc2461a7d5476ac8d44ba027e3c057
arielito <ariel.liquid@gmail.com>
2024-11-14 21:36:37 -0300

    mas action

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,7 +1,7 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Ariel',
+    firstName: 'Cristino Ariel',
     lastName:  'Ramirez',
     get name() {
         return `${this.firstName} ${this.lastName}`;
